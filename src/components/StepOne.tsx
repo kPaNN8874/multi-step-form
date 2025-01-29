7751757ebc52001689f86c8a6aa3362a2116fc8a
kelemen.panna <kelemen.panna@BURG-1E03-04>
2025-01-29 14:28:43 +0100

    Betuk

--- src/components/StepOne.tsx
+++ src/components/StepOne.tsx
@@ -10,9 +10,9 @@ const [selectedCat,setCat]=useState<string|undefined>()
 
   return (
    <> 
-    <button onClick={()=>props.changeCategory("A")} className={selectedCat=="A"? styles.selected :""}></button>
-    <button onClick={()=>props.changeCategory("B")} className={selectedCat=="B"? styles.selected :""}></button>
-    <button onClick={()=>props.changeCategory("C")} className={selectedCat=="C"? styles.selected :""}></button>
+    <button onClick={()=>props.changeCategory("A")} className={selectedCat=="A"? styles.selected :""}>A</button>
+    <button onClick={()=>props.changeCategory("B")} className={selectedCat=="B"? styles.selected :""}>B</button>
+    <button onClick={()=>props.changeCategory("C")} className={selectedCat=="C"? styles.selected :""}>C</button>
    </>
   )
 
